527c94a965dff1da483dfda7d20ba3345feb2337
mck159 <mck159@gmail.com>
2014-02-09 19:13:07 +0100

    dodana data i model comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,15 @@
 class Comment
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :value, type: Integer, default: 0
+  field :abusive, type: Boolean, default: false
+  field :body, type: String
+
+  validates_presence_of :body
+
+  belongs_to :user
+  belongs_to :post
+
+
 end
